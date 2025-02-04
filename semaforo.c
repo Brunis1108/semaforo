31dd8bc098e7a1750e042a7d51e821946a6dea70
Brunis1108 <bruna.a.ba76@gmail.com>
2025-02-04 13:06:47 -0300

    Subindo codigo

---
+++ semaforo.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/timer.h"
+
+#define LED_AMARELO 12
+#define LED_VERMELHO 13
+#define LED_VERDE 11
+
+// Variáveis globais
+int estado_semaforo = 0;
+
+// Callback para alternar o semáforo
+bool semaforo_callback(struct repeating_timer *t) {
+    gpio_put(LED_VERMELHO, estado_semaforo == 0);
+    gpio_put(LED_AMARELO, estado_semaforo == 1);
+    gpio_put(LED_VERDE, estado_semaforo == 2);
+    estado_semaforo = (estado_semaforo + 1) % 3;
+    return true; // Mantém o temporizador ativo
+}
+
+int main() {
+    stdio_init_all();
+    
+    // Configuração dos LEDs
+    gpio_init(LED_AMARELO);
+    gpio_init(LED_VERMELHO);
+    gpio_init(LED_VERDE);
+    gpio_set_dir(LED_AMARELO, GPIO_OUT);
+    gpio_set_dir(LED_VERMELHO, GPIO_OUT);
+    gpio_set_dir(LED_VERDE, GPIO_OUT);
+    
+    // Inicia temporizador periódico para o semáforo
+    struct repeating_timer timer;
+    add_repeating_timer_ms(3000, semaforo_callback, NULL, &timer);
+    
+    // Loop principal
+    while (true) {
+        printf("Semáforo em execução...\n");
+        sleep_ms(1000);
+    }
+}
